172fca54bd83aa6e99c90677add5b0b1a3999cd3
Edoardo Nasuto <edoardo.nasuto@gmail.com>
2023-04-10 15:11:55 +0200

    First commit

---
+++ trophees_nsi/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for trophees_nsi project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'trophees_nsi.settings')
+
+application = get_asgi_application()
